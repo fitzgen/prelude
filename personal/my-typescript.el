2611d8315a7f2348bce1dfca81b087620b88220a
Nick Fitzgerald <fitzgen@gmail.com>
2016-03-14 15:22:11 -0700

    commit whatever random mess I have in here

---
+++ personal/my-typescript.el
@@ -0,0 +1,9 @@
+(add-hook 'typescript-mode-hook
+          (lambda ()
+            (tide-setup)
+            (flycheck-mode t)
+            (setq flycheck-check-syntax-automatically '(save mode-enabled))
+            (eldoc-mode t)
+            ))
+
+(provide 'my-typescript)
